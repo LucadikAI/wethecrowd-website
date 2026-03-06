dccd97d21908cb31b0a1c83932281d04ad5c7566
Luca Dik <lucadik@MacBook-Pro-66.local>
2026-03-06 15:51:09 +0100

    Update

--- src/components/VenueSlider.tsx
+++ src/components/VenueSlider.tsx
@@ -32,8 +32,6 @@ export default function VenueSlider() {
   const animControl = useRef<ReturnType<typeof animate> | null>(null);
   const progressControl = useRef<ReturnType<typeof animate> | null>(null);
 
-  const indicatorLeft = useTransform(progressValue, [0, 1], ["0%", "75%"]);
-
   const startInfiniteLoop = () => {
     const hw = halfWidth.current;
     animControl.current?.stop();
